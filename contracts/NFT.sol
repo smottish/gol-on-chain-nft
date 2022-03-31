3ecc9bacd7f43aa4ce3bb473afbb1edb152eb5ab
Shane Mottishaw <shanemottishaw@gmail.com>
2022-03-30 19:44:57 -0700

    Add payment and max supply

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -3,20 +3,31 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/PullPayment.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFT is ERC721 {
+// Inheriting from PullPayment adds a `payments` function to query due
+// payments, and `withdrawPayments` to get funds out of the contract
+contract NFT is ERC721, PullPayment, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private currentTokenId;
     string public baseTokenURI;
+    uint256 public constant TOTAL_SUPPLY = 10;
+    uint256 public constant MINT_PRICE = 0.01 ether;
 
     constructor() ERC721("PracticeNFT", "NFT") {
         baseTokenURI = "";
     }
 
-    function mintTo(address recipient) public returns (uint256) {
+    function mintTo(address recipient) public payable returns (uint256) {
+        uint256 tokenId = currentTokenId.current();
+        require(tokenId < TOTAL_SUPPLY, "Max supply reached");
+        require(msg.value == MINT_PRICE, "Transaction value didn't equal the mint price");
+
         currentTokenId.increment();
         uint256 newTokenId = currentTokenId.current();
         _safeMint(recipient, newTokenId);
+        _asyncTransfer(owner(), msg.value);
         return newTokenId;
     }
 
@@ -26,7 +37,11 @@ contract NFT is ERC721 {
         return baseTokenURI;
     }
 
-    function setBaseTokenURI(string memory _baseTokenURI) public {
+    function setBaseTokenURI(string memory _baseTokenURI) public onlyOwner {
         baseTokenURI = _baseTokenURI;
+    }
+
+    function withdrawPayments(address payable payee) public override onlyOwner virtual {
+        super.withdrawPayments(payee);
     }
 }
--- test/NFT.test.js
+++ test/NFT.test.js
@@ -1,7 +1,13 @@
 const { ethers } = require("hardhat");
-const { expect } = require("chai");
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
+const expect = chai.expect;
+
+chai.use(chaiAsPromised);
 
 describe("NFT tests", function() {
+    const MINT_PRICE = ethers.utils.parseEther("0.01");
+    const MAX_SUPPLY = 10;
 
     /*
      * NOTE: If it takes too long to deploy the contract for
@@ -22,7 +28,9 @@ describe("NFT tests", function() {
     it("should mint a new NFT", async function() {
         const nft = await deployNFTContract();
         const [deployer, acct2] = await ethers.getSigners()
-        await nft.mintTo(deployer.address);
+        await nft.mintTo(deployer.address, {
+            value: MINT_PRICE,
+        });
 
         const address = await nft.ownerOf(1);
         const numOwnedByDeployer = await nft.balanceOf(deployer.address);
@@ -32,14 +40,71 @@ describe("NFT tests", function() {
         expect(numOwnedByAcct2).to.equal(0);
     })
 
+    it("should not mint an NFT if the tx value is too low", async function() {
+        const nft = await deployNFTContract();
+        const [deployer] = await ethers.getSigners()
+        await expect(nft.mintTo(deployer.address, {
+            value: MINT_PRICE.sub(ethers.utils.parseUnits("1", "wei")),
+        })).to.be.rejected;
+        expect(await nft.balanceOf(deployer.address)).to.equal(0);
+    })
+
     it("should set the base token URI", async function() {
         const BASE_TOKEN_URI = "https://example.com/"
         const nft = await deployNFTContract();
         const [deployer] = await ethers.getSigners()
 
         await nft.setBaseTokenURI(BASE_TOKEN_URI);
-        await nft.mintTo(deployer.address);
+        await nft.mintTo(deployer.address, {
+            value: MINT_PRICE,
+        });
 
         expect(await nft.tokenURI(1)).to.equal(`${BASE_TOKEN_URI}1`);
+    })
+
+    it("should let the owner withdraw funds", async function() {
+        const nft = await deployNFTContract();
+        const [deployer, acct2] = await ethers.getSigners();
+
+        expect(await nft.owner()).to.equal(deployer.address);
+        await nft.mintTo(acct2.address, {
+            value: MINT_PRICE,
+        });
+
+        const paymentDue = await nft.payments(deployer.address);
+        expect(paymentDue).to.equal(MINT_PRICE); // Waffle handles equality of BigNumbers.
+
+        // The following amount should be deposited into the owner's account:
+        // [Mint Price] - [gas fee for calling withdrawPayments]
+        const beforeBalance = await deployer.getBalance();
+        const tx = await nft.withdrawPayments(deployer.address);
+        const afterBalance = await deployer.getBalance();
+
+        const txReceipt = await tx.wait();
+        const gasFee = txReceipt.gasUsed.mul(txReceipt.effectiveGasPrice);
+        const expectedDeposit = MINT_PRICE.sub(gasFee)
+        const actualDeposit = afterBalance.sub(beforeBalance);
+        expect(actualDeposit).to.equal(expectedDeposit);
+    })
+
+    it("should not let a non-owner withdraw funds", async function() {
+        const acct2 = (await ethers.getSigners())[1];
+        let nft = await deployNFTContract();
+        nft = await nft.connect(acct2)
+        await expect(nft.withdrawPayments(acct2.address)).to.be.rejected;
+    })
+
+    it("should not mint more than 10", async function() {
+        const nft = await deployNFTContract();
+        const [deployer] = await ethers.getSigners();
+        const mint = () => nft.mintTo(deployer.address, {
+            value: MINT_PRICE,
+        });
+
+        for (let i = 0; i < MAX_SUPPLY; i++) {
+            await mint();
+        }
+
+        await expect(mint()).to.be.rejected;
     })
 })
